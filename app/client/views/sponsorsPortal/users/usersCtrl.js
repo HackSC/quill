1088d89d9b3b29c5954f164404bf61a0665ed1ee
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-02-24 12:05:24 -0800

    Set up interface for filtering

--- app/client/views/sponsorsPortal/users/usersCtrl.js
+++ app/client/views/sponsorsPortal/users/usersCtrl.js
@@ -2,5 +2,23 @@ angular.module('reg')
   .controller('SponsorsUsersCtrl', [
     '$scope',
     function($scope) {
-      $scope.test = 'baller';
+      $scope.filter = {
+        experience: {
+          beginner: true,
+          intermediate: true,
+          advanced: false
+        },
+        gender: {
+          male: true,
+          female: true
+        },
+        year: {
+          '2022': false,
+          '2021': false,
+          '2020': false,
+          '2019': false,
+          'Graduate': false
+        },
+        skills: []
+      }
     }]);
