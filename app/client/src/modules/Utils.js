24a97443458c9b47b3813b8f87cf3bf95a09c2a4
Daniel Ho <danielho54@gmail.com>
2019-04-12 02:54:36 -0700

    utils hotfix

--- app/client/src/modules/Utils.js
+++ app/client/src/modules/Utils.js
@@ -5,28 +5,35 @@ angular.module('reg')
   .factory('Utils', [
     function(){
       return {
-        isRegOpen: function(settings, email){
+        isRegOpen: function (settings, email) {
           var usc = email.split('@')[1] === 'usc.edu';
           return Date.now() > settings.timeOpen && Date.now() < (!usc ? settings.timeClose : settings.timeCloseUSC);
         },
-        isAnyRegOpen: function(settings){
+        isAnyRegOpen: function (settings) {
           return Date.now() > settings.timeOpen && Date.now() < Math.max(settings.timeClose, settings.timeCloseUSC);
         },
-        isAfter: function(time){
+        isAfter: function (time) {
           return Date.now() > time;
         },
         formatTimeShort: function (time) {
-            if (!time) {
-              return "Invalid Date";
-            }
-            date = new Date(time);
-            // Hack for timezone
-            return moment(date).format('h:mm a');
-        },
-        formatTime: function(time){
-
-          if (!time){
+          if (!time) {
             return "Invalid Date";
           }
-        };
-      }]);
+          date = new Date(time);
+          // Hack for timezone
+          return moment(date).format('h:mm a');
+        },
+        formatTime: function (time) {
+
+          if (!time) {
+            return "Invalid Date";
+          }
+
+          date = new Date(time);
+          // Hack for timezone
+          return moment(date).format('dddd, MMMM Do YYYY, h:mm a') +
+              " " + date.toTimeString().split(' ').slice(2).join(' ')
+
+        },
+      }
+    }]);
